b327f33bb53fc2229b523ec4c74c8f40347ba0b3
Programerus <alcadeias95@gmail.com>
2012-03-19 16:49:58 +0100

    Comment fix.

--- src/d3denum.cpp
+++ src/d3denum.cpp
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.//-----------------------------------------------------------------------------
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+//-----------------------------------------------------------------------------
 // File: D3DEnum.cpp
 //
 // Desc: Functions to enumerate DDraw/D3D drivers, devices, and modes.
