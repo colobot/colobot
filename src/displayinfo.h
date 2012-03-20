4a45ed2242ed898c9fc556f5ba0f7a82dd75db2f
Programerus <alcadeias95@gmail.com>
2012-03-20 22:39:02 +0100

    Comment fix.

--- src/displayinfo.h
+++ src/displayinfo.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// displayinfo.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// displayinfo.h
 
 #ifndef _DISPLAYINFO_H_
 #define	_DISPLAYINFO_H_
