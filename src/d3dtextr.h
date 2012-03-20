507108ae305aedbacba083b09a07996a4ce8abd4
Programerus <alcadeias95@gmail.com>
2012-03-20 18:42:12 +0100

    Comment fix.

--- src/d3dtextr.h
+++ src/d3dtextr.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.//-----------------------------------------------------------------------------
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+//-----------------------------------------------------------------------------
 // File: D3DTextr.h
 //
 // Desc: Functions to manage textures, including creating (loading from a
