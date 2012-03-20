2f2a1874f1671f0fc127114edaceadbffd6a7d92
Programerus <alcadeias95@gmail.com>
2012-03-20 18:36:11 +0100

    Comment fix.

--- src/d3dframe.cpp
+++ src/d3dframe.cpp
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.//-----------------------------------------------------------------------------
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+//-----------------------------------------------------------------------------
 // File: D3DFrame.cpp
 //
 // Desc: Class functions to implement a Direct3D app framework.
