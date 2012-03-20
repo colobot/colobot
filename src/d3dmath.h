6096a4f360d6057ecb5db3c84e0c1279249536c3
Programerus <alcadeias95@gmail.com>
2012-03-20 18:39:37 +0100

    Comment fix.

--- src/d3dmath.h
+++ src/d3dmath.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.//-----------------------------------------------------------------------------
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+//-----------------------------------------------------------------------------
 // File: D3DMath.h
 //
 // Desc: Math functions and shortcuts for Direct3D programming.
