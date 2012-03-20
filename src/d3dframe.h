78fd384dcd69abd8307baa8fa1698ffd113d0c52
Programerus <alcadeias95@gmail.com>
2012-03-20 18:37:21 +0100

    Comment fix.

--- src/d3dframe.h
+++ src/d3dframe.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.//-----------------------------------------------------------------------------
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+//-----------------------------------------------------------------------------
 // File: D3DFrame.h
 //
 // Desc: Class to manage the Direct3D environment objects such as buffers,
