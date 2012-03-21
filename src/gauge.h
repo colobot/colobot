025e2e55ab0fcc1a7d0d63f9667fb5043585ffe9
Programerus <alcadeias95@gmail.com>
2012-03-21 22:24:37 +0100

    Comment fix.

--- src/gauge.h
+++ src/gauge.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// gauge.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// gauge.h
 
 #ifndef _GAUGE_H_
 #define	_GAUGE_H_
