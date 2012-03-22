7e73ad95d8198f1eb81ea4b26b172db763cedb8a
Programerus <alcadeias95@gmail.com>
2012-03-22 16:27:40 +0100

    Comment fix.

--- src/image.h
+++ src/image.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// image.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// image.h
 
 #ifndef _IMAGE_H_
 #define	_IMAGE_H_
