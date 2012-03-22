fa8cc4fa1e1ac1e0ebb017f394e5993049f7211d
Programerus <alcadeias95@gmail.com>
2012-03-22 20:35:12 +0100

    Comment fix.

--- src/mainmap.h
+++ src/mainmap.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// mainmap.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// mainmap.h
 
 #ifndef _MAINMAP_H_
 #define	_MAINMAP_H_
