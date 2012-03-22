5ae68dbecf5626f628eb7cbb79537eb910e05c91
Programerus <alcadeias95@gmail.com>
2012-03-22 17:06:19 +0100

    Comment fix.

--- src/interface.h
+++ src/interface.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// interface.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// interface.h
 
 #ifndef _INTERFACE_H_
 #define	_INTERFACE_H_
