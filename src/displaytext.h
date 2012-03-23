955436f5c226cdda12ee341bb94cc9b6276e0baa
Programerus <alcadeias95@gmail.com>
2012-03-23 19:53:39 +0100

    Comment fix.

--- src/displaytext.h
+++ src/displaytext.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// displaytext.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// displaytext.h
 
 #ifndef _DISPLAYTEXT_H_
 #define	_DISPLAYTEXT_H_
