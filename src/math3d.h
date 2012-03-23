e5532f83d318eaa36f90274820c42b2175131c72
Programerus <alcadeias95@gmail.com>
2012-03-23 21:02:43 +0100

    Comment fix.

--- src/math3d.h
+++ src/math3d.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// math3d.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// math3d.h
 
 #ifndef _MATH3D_H_
 #define	_MATH3D_H_
