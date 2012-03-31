5b12ea3317369a2a318dd00630297b91a51ae535
Programerus <alcadeias95@gmail.com>
2012-04-01 01:37:19 +0300

    Comment fix.

--- src/motionhuman.h
+++ src/motionhuman.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// motionhuman.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// motionhuman.h
 
 #ifndef _MOTIONHUMAN_H_
 #define	_MOTIONHUMAN_H_
