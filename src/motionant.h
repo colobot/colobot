298c6952aaa1a86b8ba94d3e7dcc9d1d335b8ae5
Programerus <alcadeias95@gmail.com>
2012-03-31 19:00:56 +0300

    Comment fix.

--- src/motionant.h
+++ src/motionant.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// motionant.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// motionant.h
 
 #ifndef _MOTIONANT_H_
 #define	_MOTIONANT_H_
