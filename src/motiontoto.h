4f4a0676c6a8fcba278e8006274934ce3d659413
Programerus <alcadeias95@gmail.com>
2012-04-02 00:14:04 +0300

    Comment fix.

--- src/motiontoto.h
+++ src/motiontoto.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// motiontoto.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// motiontoto.h
 
 #ifndef _MOTIONTOTO_H_
 #define	_MOTIONTOTO_H_
