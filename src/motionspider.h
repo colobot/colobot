0ee02d7197035632f48721f713355374ae9640f1
Programerus <alcadeias95@gmail.com>
2012-04-01 23:44:50 +0300

    Comment fix.

--- src/motionspider.h
+++ src/motionspider.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// motionspider.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// motionspider.h
 
 #ifndef _MOTIONSPIDER_H_
 #define	_MOTIONSPIDER_H_
