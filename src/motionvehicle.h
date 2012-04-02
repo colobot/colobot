87a8bcf9a21a96a87045e197e0dcc13d79bab0b0
Programerus <alcadeias95@gmail.com>
2012-04-02 22:48:47 +0300

    Comment fix.

--- src/motionvehicle.h
+++ src/motionvehicle.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// motionvehicle.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// motionvehicle.h
 
 #ifndef _MOTIONVEHICLE_H_
 #define	_MOTIONVEHICLE_H_
