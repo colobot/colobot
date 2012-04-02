4e48188bdaa3a8081a7dbc3b7d8594fe8132c165
Programerus <alcadeias95@gmail.com>
2012-04-02 23:00:17 +0300

    Comment fix.

--- src/motionworm.h
+++ src/motionworm.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// motionworm.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// motionworm.h
 
 #ifndef _MOTIONWORM_H_
 #define	_MOTIONWORM_H_
