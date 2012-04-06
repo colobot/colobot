155c6c151cfbbc214a7a3e75e9586cc1a6a26fa0
Programerus <alcadeias95@gmail.com>
2012-04-06 22:12:38 +0300

    Comment fix.

--- src/profile.cpp
+++ src/profile.cpp
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// profile.cpp
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// profile.cpp
 
 #define STRICT
 #define D3D_OVERLOADS
