8c0dd5ced2b697a95c65caad32dfda1d41ce2091
Programerus <alcadeias95@gmail.com>
2012-04-06 22:13:01 +0300

    Comment fix.

--- src/profile.h
+++ src/profile.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// profile.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// profile.h
 
 #ifndef _PROFILE_H_
 #define	_PROFILE_H_
