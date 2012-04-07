b1bf48e9fdafbd5783099ef1bf7d207f2bdcdba6
Programerus <alcadeias95@gmail.com>
2012-04-07 16:30:51 +0300

    Comment fix.

--- src/resource.h
+++ src/resource.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.//{{NO_DEPENDENCIES}}
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+//{{NO_DEPENDENCIES}}
 // Microsoft Developer Studio generated include file.
 // Used by winmain.rc
 //
