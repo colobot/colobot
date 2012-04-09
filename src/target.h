265f6942414b2275acb3b01f2e2faa48af77e7f8
Programerus <alcadeias95@gmail.com>
2012-04-10 00:03:33 +0300

    Comment fix.

--- src/target.h
+++ src/target.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// target.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// target.h
 
 #ifndef _TARGET_H_
 #define	_TARGET_H_
