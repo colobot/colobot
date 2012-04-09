079f5abd8ba3f466e39f0cfc7be3d1bf3dcb82bc
Programerus <alcadeias95@gmail.com>
2012-04-09 23:59:27 +0300

    Comment fix.

--- src/studio.h
+++ src/studio.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// studio.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// studio.h
 
 #ifndef _STUDIO_H_
 #define	_STUDIO_H_
