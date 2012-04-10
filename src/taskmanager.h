3bb578a773290cbad4667d5a6b3682d5826df9b9
Programerus <alcadeias95@gmail.com>
2012-04-10 19:29:25 +0300

    Comment fix.

--- src/taskmanager.h
+++ src/taskmanager.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// taskmanager.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// taskmanager.h
 
 #ifndef _TASKMANAGER_H_
 #define	_TASKMANAGER_H_
