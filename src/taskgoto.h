7d508e9cb92698523bc62a4561bd41fd248ee61a
Programerus <alcadeias95@gmail.com>
2012-04-10 18:57:16 +0300

    Comments translated from French to English.

--- src/taskgoto.h
+++ src/taskgoto.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// taskgoto.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// taskgoto.h
 
 #ifndef _TASKGOTO_H_
 #define	_TASKGOTO_H_
@@ -32,41 +34,41 @@ class CObject;
 
 enum TaskGotoGoal
 {
-	TGG_DEFAULT		= -1,	// mode par défaut
-	TGG_STOP		= 0,	// va à destination en s'arrêtant avec précision
-	TGG_EXPRESS		= 1,	// va à destination sans s'arrêter
+	TGG_DEFAULT		= -1,	// default mode
+	TGG_STOP		= 0,	// goes to destination pausing with precision
+	TGG_EXPRESS		= 1,	// goes to destination without stopping
 };
 
 enum TaskGotoCrash
 {
-	TGC_DEFAULT		= -1,	// mode par défaut
-	TGC_HALT		= 0,	// stoppe si collision
-	TGC_RIGHTLEFT	= 1,	// droite-gauche
-	TGC_LEFTRIGHT	= 2,	// gauche-droite
-	TGC_LEFT		= 3,	// gauche
-	TGC_RIGHT		= 4,	// droite
-	TGC_BEAM		= 5,	// algorithme "rayons de soleil"
+	TGC_DEFAULT		= -1,	// default mode
+	TGC_HALT		= 0,	// stops if collision
+	TGC_RIGHTLEFT		= 1,	// right-left
+	TGC_LEFTRIGHT		= 2,	// left-right
+	TGC_LEFT		= 3,	// left
+	TGC_RIGHT		= 4,	// right
+	TGC_BEAM		= 5,	// algorithm "sunlight"
 };
 
 
 enum TaskGotoPhase
 {
-	TGP_ADVANCE		= 1,	// avance
-	TGP_LAND		= 2,	// atterri
-	TGP_TURN		= 3,	// tourne pour finir
-	TGP_MOVE		= 4,	// avance pour finir
-	TGP_CRWAIT		= 5,	// attend après collision
-	TGP_CRTURN		= 6,	// tourne à droite après collision
-	TGP_CRADVANCE	= 7,	// avance à droite après collision
-	TGP_CLWAIT		= 8,	// attend après collision
-	TGP_CLTURN		= 9,	// tourne à gauche après collision
-	TGP_CLADVANCE	= 10,	// avance à gauche après collision
-	TGP_BEAMLEAK	= 11,	// beam: leak (fuite)
-	TGP_BEAMSEARCH	= 12,	// beam: search
-	TGP_BEAMWCOLD	= 13,	// beam: attend refroidissement réacteur
-	TGP_BEAMUP		= 14,	// beam: décolle
-	TGP_BEAMGOTO	= 15,	// beam: goto dot list
-	TGP_BEAMDOWN	= 16,	// beam: atterri
+	TGP_ADVANCE		= 1,	// advance
+	TGP_LAND		= 2,	// landed
+	TGP_TURN		= 3,	// turns to finish
+	TGP_MOVE		= 4,	// advance to finish
+	TGP_CRWAIT		= 5,	// waits after collision
+	TGP_CRTURN		= 6,	// turns right after collision
+	TGP_CRADVANCE		= 7,	// advance to the right after collision
+	TGP_CLWAIT		= 8,	// waits after collision
+	TGP_CLTURN		= 9,	// turns left after collision
+	TGP_CLADVANCE		= 10,	// advance to the left after collision
+	TGP_BEAMLEAK		= 11,	// beam: leak (leaking)
+	TGP_BEAMSEARCH		= 12,	// beam: search
+	TGP_BEAMWCOLD		= 13,	// beam: expects cool reactor
+	TGP_BEAMUP		= 14,	// beam: off
+	TGP_BEAMGOTO		= 15,	// beam: goto dot list
+	TGP_BEAMDOWN		= 16,	// beam: landed
 };
 
 
@@ -124,7 +126,7 @@ protected:
 	int				m_try;
 	Error			m_error;
 	BOOL			m_bTake;
-	float			m_stopLength;	// distance de freinage
+	float			m_stopLength;	// braking distance
 	float			m_time;
 	D3DVECTOR		m_pos;
 	BOOL			m_bWorm;
@@ -132,26 +134,26 @@ protected:
 	float			m_wormLastTime;
 	float			m_lastDistance;
 
-	int				m_bmSize;		// largeur ou hauteur du tableau
+	int				m_bmSize;		// width or height of the table
 	int				m_bmOffset;		// m_bmSize/2
-	int				m_bmLine;		// incrément ligne m_bmSize/8
-	unsigned char*	m_bmArray;		// tableau de bits
+	int				m_bmLine;		// increment line m_bmSize/8
+	unsigned char*			m_bmArray;		// bit table
 	int				m_bmMinX, m_bmMinY;
 	int				m_bmMaxX, m_bmMaxY;
-	int				m_bmTotal;		// nb de points dans m_bmPoints
-	int				m_bmIndex;		// index dans m_bmPoints
+	int				m_bmTotal;		// number of points in m_bmPoints
+	int				m_bmIndex;		// index in m_bmPoints
 	D3DVECTOR		m_bmPoints[MAXPOINTS+2];
 	char			m_bmIter[MAXPOINTS+2];
 	int				m_bmIterCounter;
 	CObject*		m_bmFretObject;
-	float			m_bmFinalMove;	// distance finale à avancer
-	float			m_bmFinalDist;	// distance effective à avancer
-	D3DVECTOR		m_bmFinalPos;	// position initiale avant avance
+	float			m_bmFinalMove;	// final advance distance
+	float			m_bmFinalDist;	// effective distance to advance
+	D3DVECTOR		m_bmFinalPos;	// initial position before advance
 	float			m_bmTimeLimit;
 	int				m_bmStep;
 	D3DVECTOR		m_bmWatchDogPos;
 	float			m_bmWatchDogTime;
-	D3DVECTOR		m_leakPos;		// position initiale à fuire
+	D3DVECTOR		m_leakPos;		// initial position leak
 	float			m_leakDelay;
 	float			m_leakTime;
 	BOOL			m_bLeakRecede;
