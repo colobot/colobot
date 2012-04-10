8d7dfc5c3e9efaadd55f943d10d08dfc870675a1
Programerus <alcadeias95@gmail.com>
2012-04-10 20:49:35 +0300

    Comment fix.

--- src/taskspiderexplo.h
+++ src/taskspiderexplo.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// taskspiderexplo.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// taskspiderexplo.h
 
 #ifndef _TASKSPIDEREXPLO_H_
 #define	_TASKSPIDEREXPLO_H_
