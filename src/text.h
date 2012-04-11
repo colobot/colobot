5cd7896e1a8d729042e7a81d6621bee094fa10b7
Programerus <alcadeias95@gmail.com>
2012-04-11 21:12:58 +0300

    Comment fix.

--- src/text.h
+++ src/text.h
@@ -12,7 +12,9 @@
 // * GNU General Public License for more details.
 // *
 // * You should have received a copy of the GNU General Public License
-// * along with this program. If not, see  http://www.gnu.org/licenses/.// text.h
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// text.h
 
 #ifndef _TEXT_H_
 #define	_TEXT_H_
