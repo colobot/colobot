0ce1436ad2506c98589524fd45292ebc791c8bab
Piotr Dziwinski <piotrdz@gmail.com>
2012-04-28 21:46:26 +0200

    Split math functions; beginning of common structures

---
+++ src/math/point.h
@@ -0,0 +1,19 @@
+#pragma once
+
+/* TODO
+FPOINT    RotatePoint(FPOINT center, float angle, FPOINT p);
+FPOINT    RotatePoint(float angle, FPOINT p);
+FPOINT    RotatePoint(float angle, float dist);
+void      RotatePoint(float cx, float cy, float angle, float &px, float &py);
+void      RotatePoint(D3DVECTOR center, float angleH, float angleV, D3DVECTOR &p);
+void      RotatePoint2(D3DVECTOR center, float angleH, float angleV, D3DVECTOR &p);
+float   Length(FPOINT a, FPOINT b);
+
+float   RotateAngle(float x, float y);
+float   RotateAngle(FPOINT center, FPOINT p1, FPOINT p2);
+float   MidPoint(FPOINT a, FPOINT b, float px);
+BOOL      IsInsideTriangle(FPOINT a, FPOINT b, FPOINT c, FPOINT p);
+
+BOOL      LineFunction(FPOINT p1, FPOINT p2, float &a, float &b);
+
+*/
