ecab9761d46754cd8d7a513a8ac16a8b88499d55
erihel <erihel@gmail.com>
2012-07-04 19:38:18 +0200

    * Added OpenAL sound plugin
    Plugin needs some more work and tests. It doesn't support playing music
    yet as sound interface is propably going to change a bit.

---
+++ src/plugins/sound/oalsound/alsound.h
@@ -0,0 +1,94 @@
+// * This file is part of the COLOBOT source code
+// * Copyright (C) 2001-2008, Daniel ROUX & EPSITEC SA, www.epsitec.ch
+// * Copyright (C) 2012, Polish Portal of Colobot (PPC)
+// *
+// * This program is free software: you can redistribute it and/or modify
+// * it under the terms of the GNU General Public License as published by
+// * the Free Software Foundation, either version 3 of the License, or
+// * (at your option) any later version.
+// *
+// * This program is distributed in the hope that it will be useful,
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// * GNU General Public License for more details.
+// *
+// * You should have received a copy of the GNU General Public License
+// * along with this program. If not, see  http://www.gnu.org/licenses/.
+
+// alsound.h
+
+#pragma once
+
+#include <map>
+#include <string>
+
+#include <AL/alut.h>
+
+#include <common/iman.h>
+#include <common/logger.h>
+#include <sound/sound.h>
+
+#include "buffer.h"
+#include "channel.h"
+#include "check.h"
+
+
+class ALSound : public CSoundInterface
+{
+    public:
+        ALSound();
+        ~ALSound();
+
+        bool Create(bool b3D);
+        void CacheAll();
+        bool Cache(Sound, std::string);
+
+        bool RetEnable();
+
+        void SetSound3D(bool bMode);
+        bool RetSound3D();
+        bool RetSound3DCap();
+
+        void SetAudioVolume(int volume);
+        int RetAudioVolume();
+        void SetMusicVolume(int volume);
+        int RetMusicVolume();
+
+        void SetListener(Math::Vector eye, Math::Vector lookat);
+        void FrameMove(float rTime);
+
+        int Play(Sound sound, float amplitude=1.0f, float frequency=1.0f, bool bLoop = false);
+        int Play(Sound sound, Math::Vector pos, float amplitude=1.0f, float frequency=1.0f, bool bLoop = false);
+        bool FlushEnvelope(int channel);
+        bool AddEnvelope(int channel, float amplitude, float frequency, float time, SoundNext oper);
+        bool Position(int channel, Math::Vector pos);
+        bool Frequency(int channel, float frequency);
+        bool Stop(int channel);
+        bool StopAll();
+        bool MuteAll(bool bMute);
+
+        bool PlayMusic(int rank, bool bRepeat);
+        bool RestartMusic();
+        void SuspendMusic();
+        void StopMusic();
+        bool IsPlayingMusic();
+
+        // plugin interface
+        char* pluginName();
+        int pluginVersion();
+        void pluginInit();
+  
+    private:
+        void CleanUp();
+        int RetPriority(Sound);
+        bool SearchFreeBuffer(Sound sound, int &channel, bool &bAlreadyLoaded);
+
+        bool mEnabled;
+        bool m3D;
+        bool mMute;
+        int mAudioVolume;
+        ALCdevice* audioDevice;
+        ALCcontext* audioContext;
+        std::map<Sound, Buffer*> mSounds;
+        std::map<int, Channel*> mChannels;
+};
