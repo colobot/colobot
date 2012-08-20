58c2e5fb7efa9b8ddec046df43930756e81db14b
Michał Konopacki <konopacki.m@gmail.com>
2012-08-20 21:45:17 +0200

    Typo fix

--- src/ui/slider.cpp
+++ src/ui/slider.cpp
@@ -30,7 +30,7 @@
 #include "common/misc.h"
 #include "common/iman.h"
 //#include "old/text.h"
-#include "graphics/engine.text.h"
+#include "graphics/engine/text.h"
 #include "ui/button.h"
 #include "ui/slider.h"
 
