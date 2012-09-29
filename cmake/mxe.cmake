e7e895438497f4efcb4d8bee240b2fe4e5938184
Piotr Dziwinski <piotrdz@gmail.com>
2012-09-29 17:19:23 +0200

    MXE support and CMake files refactoring
     - added support for cross-compiling with MXE (http://mxe.cc/)
     - refactored CMake files, adding some options and moving definitions
       to more suitable places

---
+++ cmake/mxe.cmake
@@ -0,0 +1,29 @@
+# When cross-compiling with MXE, we need to straighten some things
+
+# Checking is a bit primitive, but this should detect MXE toolchain file
+if((${CMAKE_CROSSCOMPILING}) AND (DEFINED MSYS))
+    message(STATUS "Detected MXE build")
+    set(MXE 1)
+    # Because some tests will not compile
+    set(TESTS OFF)
+    # All must be static, CBOT and GLEW too
+    set(CBOT_STATIC ON)
+    set(GLEW_STATIC ON)
+    # Because find package scripts are lame
+    set(SDLTTF_INCLUDE_DIR ${CMAKE_FIND_ROOT_PATH}/include/SDL)
+    set(SDLIMAGE_INCLUDE_DIR ${CMAKE_FIND_ROOT_PATH}/include/SDL)
+    set(MXE_LIBS
+      # For some reason, these have to be absolute paths
+      ${CMAKE_FIND_ROOT_PATH}/lib/libintl.a
+      ${CMAKE_FIND_ROOT_PATH}/lib/libiconv.a
+      ${CMAKE_FIND_ROOT_PATH}/lib/libglew32s.a
+      ${CMAKE_FIND_ROOT_PATH}/lib/libfreetype.a
+      ${CMAKE_FIND_ROOT_PATH}/lib/libltdl.a
+      ${CMAKE_FIND_ROOT_PATH}/lib/libopengl32.a
+      ${CMAKE_FIND_ROOT_PATH}/lib/libjpeg.a
+      ${CMAKE_FIND_ROOT_PATH}/lib/libwinmm.a
+      ${CMAKE_FIND_ROOT_PATH}/lib/libdxguid.a
+    )
+else()
+    set(MXE 0)
+endif()
--- src/common/config.h.cmake
+++ src/common/config.h.cmake
@@ -1,10 +1,9 @@
 #pragma once
 
 // Macros set by CMake
-#cmakedefine DEBUG
-
 #cmakedefine PLATFORM_WINDOWS @PLATFORM_WINDOWS@
 #cmakedefine PLATFORM_LINUX @PLATFORM_LINUX@
 #cmakedefine PLATFORM_OTHER @PLATFORM_OTHER@
 
 #cmakedefine USE_GLEW @USE_GLEW@
+#cmakedefine GLEW_STATIC
