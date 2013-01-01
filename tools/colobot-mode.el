a66f3f757f2c194b9a26d590a5a5ac97e54b5195
Michał Zieliński <michal@zielinscy.org.pl>
2013-01-01 21:29:56 +0100

    colobot-mode.el for Emacs

---
+++ tools/colobot-mode.el
@@ -0,0 +1,9 @@
+(defun colobot-mode ()
+  "CBot mode"
+  (interactive)
+  (c-mode)
+  (delete-trailing-whitespace)
+  (indent-region (point-min) (point-max) nil)
+  (untabify (point-min) (point-max)))
+
+(add-to-list 'auto-mode-alist '("^/tmp/colobot_edit_" . colobot-mode))
