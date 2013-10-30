11dd911faf9763382431162985b8363a39ffe481
Didier 'OdyX' Raboud <didier@raboud.com>
2013-10-30 17:08:12 +0100

    Under MacOSX, generate an Info.plist

---
+++ desktop/Info.plist.cmake
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>CFBundleInfoDictionaryVersion</key>
+    <string>6.0</string>
+    <key>CFBundleDevelopmentRegion</key>
+    <string>English</string>
+
+    <key>CFBundleDisplayName</key>
+    <string>Colobot</string>
+    <key>CFBundleExecutable</key>
+    <string>colobot</string>
+    <key>CFBundleIconFile</key>
+    <string>Colobot</string>
+    <key>CFBundleIdentifier</key>
+    <string>info.colobot.colobot</string>
+    <key>CFBundleName</key>
+    <string>Colobot</string>
+    <key>CFBundlePackageType</key>
+    <string>APPL</string>
+    <key>CFBundleSignature</key>
+    <string>cbot</string>
+    <key>CFBundleVersion</key>
+    <string>@COLOBOT_VERSION_FULL@</string>
+  </dict>
+</plist>
