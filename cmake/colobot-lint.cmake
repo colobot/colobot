73536290b2319f7976afa3f5fbc7dd8b034975d0
Piotr Dziwinski <piotrdz@gmail.com>
2015-07-19 18:11:12 +0200

    Fix for incorrect compile flags in colobot-lint

--- cmake/colobot-lint.cmake
+++ cmake/colobot-lint.cmake
@@ -1,15 +1,17 @@
 # Some functions for use with colobot-lint
 
-option(COLOBOT_LINT_BUILD "Enable some additional functions for use with colobot-lint")
-
-if(COLOBOT_LINT_BUILD)
-
+##
+# This adds a new target with generated fake source files that include single header files
+# It is a workaround for how CMake and Clang handle header files in compilation database
+# And we need that to check each header file in the project exactly once, the same as .cpp modules
+##
+macro(add_fake_header_sources subdir)
     set(all_fake_header_src_files "")
 
     set(fake_headers_src_dir ${colobot_BINARY_DIR}/fake_header_sources)
     file(MAKE_DIRECTORY ${fake_headers_src_dir})
 
-    file(GLOB_RECURSE all_header_files RELATIVE ${colobot_SOURCE_DIR} src/*.h test/*.h)
+    file(GLOB_RECURSE all_header_files RELATIVE ${colobot_SOURCE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/*.h)
 
     foreach(header_file ${all_header_files})
         string(REGEX REPLACE "\\.h$" ".cpp" fake_header_src_file "${fake_headers_src_dir}/${header_file}")
@@ -23,6 +25,5 @@ if(COLOBOT_LINT_BUILD)
     endforeach()
 
     include_directories(${colobot_SOURCE_DIR})
-    add_library(colobot_fake_header_srcs STATIC ${all_fake_header_src_files})
-
-endif()
+    add_library(colobot_${subdir}_fake_header_srcs STATIC ${all_fake_header_src_files})
+endmacro()
