colobot/colobot

commit 083f21652949d48fc1c9dada0cfb8937e9ee2b71

Team object recoloring (#514)

src/object/old_object.cpp
@@ -27,6 +27,7 @@
 #include "common/global.h"
 #include "common/make_unique.h"
 #include "common/restext.h"
+#include "common/stringutils.h"
 
 #include "graphics/engine/lightman.h"
 #include "graphics/engine/lightning.h"
@@ -710,7 +711,7 @@ bool COldObject::ExplodeObject(ExplosionType type, float force, float decay)
     }
     m_main->RemoveFromSelectionHistory(this);
 
-    SetTeam(0); // Back to neutral on destruction
+    m_team = 0; // Back to neutral on destruction
 
     if ( m_botVar != 0 )
     {
@@ -2435,6 +2436,7 @@ bool COldObject::GetClip()
 
 void COldObject::SetTeam(int team)
 {
+    // NOTE: This shouldn't be called after the object is already created
     m_team = team;
 }
 

src/object/robotmain.cpp
@@ -35,6 +35,7 @@
 #include "common/misc.h"
 #include "common/config_file.h"
 #include "common/restext.h"
+#include "common/stringutils.h"
 
 #include "common/resources/resourcemanager.h"
 #include "common/resources/inputstream.h"
@@ -2898,7 +2899,8 @@ void CRobotMain::CreateScene(bool soluce, bool fixScene, bool resetObject)
         m_colorRefBot.g = 166.0f/256.0f;
         m_colorRefBot.b = 254.0f/256.0f;  // blue
         m_colorRefBot.a = 0.0f;
-        m_colorNewBot = m_colorRefBot;
+        m_colorNewBot.clear();
+        m_colorNewBot[0] = m_colorRefBot;
 
         m_colorRefAlien.r = 135.0f/256.0f;
         m_colorRefAlien.g = 170.0f/256.0f;
@@ -3148,7 +3150,7 @@ void CRobotMain::CreateScene(bool soluce, bool fixScene, bool resetObject)
 
             if (line->GetCommand() == "VehicleColor" && !resetObject)
             {
-                m_colorNewBot = line->GetParam("color")->AsColor(Gfx::Color(0.533f, 0.533f, 0.533f, 0.533f));
+                m_colorNewBot[line->GetParam("team")->AsInt(0)] = line->GetParam("color")->AsColor(Gfx::Color(0.533f, 0.533f, 0.533f, 0.533f));
                 continue;
             }
 
@@ -3436,6 +3438,9 @@ void CRobotMain::CreateScene(bool soluce, bool fixScene, bool resetObject)
                 InitEye();
                 SetMovieLock(false);
 
+                if(!resetObject)
+                    ChangeColor();  // changes the colors of texture
+
                 if (read[0] != 0)  // loading file ?
                     sel = IOReadScene(read, stack);
 
@@ -3493,7 +3498,7 @@ void CRobotMain::CreateScene(bool soluce, bool fixScene, bool resetObject)
                 CObject* obj = nullptr;
                 try
                 {
-                        obj = m_objMan->CreateObject(
+                    obj = m_objMan->CreateObject(
                         pos, dirAngle,
                         type,
                         line->GetParam("power")->AsFloat(1.0f),
@@ -3501,7 +3506,8 @@ void CRobotMain::CreateScene(bool soluce, bool fixScene, bool resetObject)
                         line->GetParam("h")->AsFloat(0.0f),
                         trainer = line->GetParam("trainer")->AsBool(false),
                         line->GetParam("toy")->AsBool(false),
-                        line->GetParam("option")->AsInt(0)
+                        line->GetParam("option")->AsInt(0),
+                        line->GetParam("team")->AsInt(0)
                     );
                 }
                 catch (const CObjectCreateException& e)
@@ -3578,7 +3584,6 @@ void CRobotMain::CreateScene(bool soluce, bool fixScene, bool resetObject)
                     oldObj->SetRange(line->GetParam("range")->AsFloat(30.0f));
                     oldObj->SetShield(line->GetParam("shield")->AsFloat(1.0f));
                     oldObj->SetMagnifyDamage(line->GetParam("magnifyDamage")->AsFloat(1.0f));
-                    oldObj->SetTeam(line->GetParam("team")->AsInt(0));
                     oldObj->SetClip(line->GetParam("clip")->AsBool(true));
                     oldObj->SetCheckToken(!line->GetParam("checkToken")->IsDefined() ? trainer || !selectable : line->GetParam("checkToken")->AsBool(true));
                     // SetManual will affect bot speed
@@ -3908,7 +3913,6 @@ void CRobotMain::CreateScene(bool soluce, bool fixScene, bool resetObject)
 
         if (!resetObject)
         {
-            ChangeColor();  // changes the colors of texture
             m_short->SetMode(false);  // vehicles?
         }
 
@@ -4028,6 +4032,8 @@ void CRobotMain::ChangeColor()
         m_phase != PHASE_LOST     &&
         m_phase != PHASE_APPERANCE ) return;
 
+    // Player texture
+
     Math::Point ts = Math::Point(0.0f, 0.0f);
     Math::Point ti = Math::Point(1.0f, 1.0f);  // the entire image
 
@@ -4108,27 +4114,39 @@ void CRobotMain::ChangeColor()
     colorNew2.g = 0.0f;
     colorNew2.b = 0.0f;
 
-    m_engine->ChangeTextureColor("textures/objects/base1.png",   m_colorRefBot, m_colorNewBot, colorRef2, colorNew2, 0.10f, -1.0f, ts, ti, 0, 0, true);
-    m_engine->ChangeTextureColor("textures/objects/convert.png", m_colorRefBot, m_colorNewBot, colorRef2, colorNew2, 0.10f, -1.0f, ts, ti, 0, 0, true);
-    m_engine->ChangeTextureColor("textures/objects/derrick.png", m_colorRefBot, m_colorNewBot, colorRef2, colorNew2, 0.10f, -1.0f, ts, ti, 0, 0, true);
-    m_engine->ChangeTextureColor("textures/objects/factory.png", m_colorRefBot, m_colorNewBot, colorRef2, colorNew2, 0.10f, -1.0f, ts, ti, 0, 0, true);
-    m_engine->ChangeTextureColor("textures/objects/lemt.png",    m_colorRefBot, m_colorNewBot, colorRef2, colorNew2, 0.10f, -1.0f, ts, ti, 0, 0, true);
-    m_engine->ChangeTextureColor("textures/objects/roller.png",  m_colorRefBot, m_colorNewBot, colorRef2, colorNew2, 0.10f, -1.0f, ts, ti, 0, 0, true);
-    m_engine->ChangeTextureColor("textures/objects/search.png",  m_colorRefBot, m_colorNewBot, colorRef2, colorNew2, 0.10f, -1.0f, ts, ti, 0, 0, true);
+    // VehicleColor
 
-    exclu[0] = Math::Point(  0.0f/256.0f, 160.0f/256.0f);
-    exclu[1] = Math::Point(256.0f/256.0f, 256.0f/256.0f);  // pencils
-    exclu[2] = Math::Point(0.0f, 0.0f);
-    exclu[3] = Math::Point(0.0f, 0.0f);  // terminator
-    m_engine->ChangeTextureColor("textures/objects/drawer.png",  m_colorRefBot, m_colorNewBot, colorRef2, colorNew2, 0.10f, -1.0f, ts, ti, exclu, 0, true);
+    for(auto it : m_colorNewBot)
+    {
+        int team = it.first;
+        Gfx::Color newColor = it.second;
+        std::string teamStr = StrUtils::ToString<int>(team);
+        if(team == 0) teamStr = "";
 
-    exclu[0] = Math::Point(237.0f/256.0f, 176.0f/256.0f);
-    exclu[1] = Math::Point(256.0f/256.0f, 220.0f/256.0f);  // blue canister
-    exclu[2] = Math::Point(106.0f/256.0f, 150.0f/256.0f);
-    exclu[3] = Math::Point(130.0f/256.0f, 214.0f/256.0f);  // safe location
-    exclu[4] = Math::Point(0.0f, 0.0f);
-    exclu[5] = Math::Point(0.0f, 0.0f);  // terminator
-    m_engine->ChangeTextureColor("textures/objects/subm.png",    m_colorRefBot, m_colorNewBot, colorRef2, colorNew2, 0.10f, -1.0f, ts, ti, exclu, 0, true);
+        m_engine->ChangeTextureColor("textures/objects/base1.png"+teamStr,   "textures/objects/base1.png",   m_colorRefBot, newColor, colorRef2, colorNew2, 0.10f, -1.0f, ts, ti, 0, 0, true);
+        m_engine->ChangeTextureColor("textures/objects/convert.png"+teamStr, "textures/objects/convert.png", m_colorRefBot, newColor, colorRef2, colorNew2, 0.10f, -1.0f, ts, ti, 0, 0, true);
+        m_engine->ChangeTextureColor("textures/objects/derrick.png"+teamStr, "textures/objects/derrick.png", m_colorRefBot, newColor, colorRef2, colorNew2, 0.10f, -1.0f, ts, ti, 0, 0, true);
+        m_engine->ChangeTextureColor("textures/objects/factory.png"+teamStr, "textures/objects/factory.png", m_colorRefBot, newColor, colorRef2, colorNew2, 0.10f, -1.0f, ts, ti, 0, 0, true);
+        m_engine->ChangeTextureColor("textures/objects/lemt.png"+teamStr,    "textures/objects/lemt.png",    m_colorRefBot, newColor, colorRef2, colorNew2, 0.10f, -1.0f, ts, ti, 0, 0, true);
+        m_engine->ChangeTextureColor("textures/objects/roller.png"+teamStr,  "textures/objects/roller.png",  m_colorRefBot, newColor, colorRef2, colorNew2, 0.10f, -1.0f, ts, ti, 0, 0, true);
+        m_engine->ChangeTextureColor("textures/objects/search.png"+teamStr,  "textures/objects/search.png",  m_colorRefBot, newColor, colorRef2, colorNew2, 0.10f, -1.0f, ts, ti, 0, 0, true);
+
+        exclu[0] = Math::Point(  0.0f/256.0f, 160.0f/256.0f);
+        exclu[1] = Math::Point(256.0f/256.0f, 256.0f/256.0f);  // pencils
+        exclu[2] = Math::Point(0.0f, 0.0f);
+        exclu[3] = Math::Point(0.0f, 0.0f);  // terminator
+        m_engine->ChangeTextureColor("textures/objects/drawer.png"+teamStr, "textures/objects/drawer.png",  m_colorRefBot, newColor, colorRef2, colorNew2, 0.10f, -1.0f, ts, ti, exclu, 0, true);
+
+        exclu[0] = Math::Point(237.0f/256.0f, 176.0f/256.0f);
+        exclu[1] = Math::Point(256.0f/256.0f, 220.0f/256.0f);  // blue canister
+        exclu[2] = Math::Point(106.0f/256.0f, 150.0f/256.0f);
+        exclu[3] = Math::Point(130.0f/256.0f, 214.0f/256.0f);  // safe location
+        exclu[4] = Math::Point(0.0f, 0.0f);
+        exclu[5] = Math::Point(0.0f, 0.0f);  // terminator
+        m_engine->ChangeTextureColor("textures/objects/subm.png"+teamStr,   "textures/objects/subm.png",    m_colorRefBot, newColor, colorRef2, colorNew2, 0.10f, -1.0f, ts, ti, exclu, 0, true);
+    }
+
+    // AlienColor
 
     exclu[0] = Math::Point(128.0f/256.0f, 160.0f/256.0f);
     exclu[1] = Math::Point(256.0f/256.0f, 256.0f/256.0f);  // SatCom
@@ -4137,7 +4155,10 @@ void CRobotMain::ChangeColor()
     m_engine->ChangeTextureColor("textures/objects/ant.png",     m_colorRefAlien, m_colorNewAlien, colorRef2, colorNew2, 0.50f, -1.0f, ts, ti, exclu);
     m_engine->ChangeTextureColor("textures/objects/mother.png",  m_colorRefAlien, m_colorNewAlien, colorRef2, colorNew2, 0.50f, -1.0f, ts, ti);
 
+    // GreeneryColor
     m_engine->ChangeTextureColor("textures/objects/plant.png",   m_colorRefGreen, m_colorNewGreen, colorRef2, colorNew2, 0.50f, -1.0f, ts, ti);
+
+    // water color
 
     // PARTIPLOUF0 and PARTIDROP :
     ts = Math::Point(0.500f, 0.500f);
@@ -4148,6 +4169,9 @@ void CRobotMain::ChangeColor()
     ts = Math::Point(0.00f, 0.75f);
     ti = Math::Point(0.25f, 1.00f);
     m_engine->ChangeTextureColor("textures/effect02.png", m_colorRefWater, m_colorNewWater, colorRef2, colorNew2, 0.20f, -1.0f, ts, ti, 0, m_colorShiftWater, true);
+
+    // This loads the newly recolored textures to objects
+    m_engine->LoadAllTextures();
 }
 
 //! Updates the number of unnecessary objects
@@ -6071,6 +6095,13 @@ const std::string& CRobotMain::GetTeamName(int id)
 {
     if(m_teamNames.count(id) == 0) return NO_TEAM_NAME;
     return m_teamNames[id];
+}
+
+bool CRobotMain::IsTeamColorDefined(int id)
+{
+    if(id == 0) return false; // Always use default for team 0
+
+    return m_colorNewBot.find(id) != m_colorNewBot.end();
 }
 
 

src/app/app.cpp
@@ -746,7 +746,6 @@ bool CApplication::ChangeVideoConfig(const Gfx::DeviceConfig &newConfig)
     m_device->ConfigChanged(m_deviceConfig);
 
     m_engine->ResetAfterDeviceChanged();
-    m_controller->GetRobotMain()->ResetAfterDeviceChanged();
 
     return true;
 }

src/graphics/engine/engine.cpp
@@ -380,6 +380,8 @@ void CEngine::ResetAfterDeviceChanged()
 
     FlushTextureCache();
 
+    CRobotMain::GetInstancePointer()->ResetAfterDeviceChanged();
+
     LoadAllTextures();
 }
 
@@ -2321,6 +2323,7 @@ bool IsExcludeColor(Math::Point *exclude, int x, int y)
 
 
 bool CEngine::ChangeTextureColor(const std::string& texName,
+                                 const std::string& srcName,
                                  Color colorRef1, Color colorNew1,
                                  Color colorRef2, Color colorNew2,
                                  float tolerance1, float tolerance2,
@@ -2330,11 +2333,11 @@ bool CEngine::ChangeTextureColor(const std::string& texName,
     DeleteTexture(texName);
 
     CImage img;
-    if (!img.Load(texName))
+    if (!img.Load(srcName))
     {
         std::string error = img.GetError();
-        GetLogger()->Error("Couldn't load texture '%s': %s, blacklisting\n", texName.c_str(), error.c_str());
-        m_texBlacklist.insert(texName);
+        GetLogger()->Error("Couldn't load texture '%s': %s, blacklisting\n", srcName.c_str(), error.c_str());
+        m_texBlacklist.insert(srcName);
         return false;
     }
 
@@ -2447,10 +2450,17 @@ bool CEngine::ChangeTextureColor(const std::string& texName,
     m_texNameMap[texName] = tex;
     m_revTexNameMap[tex] = texName;
 
-    // This updates the texture to updated Texture struct on the objects
-    LoadAllTextures();
-
     return true;
+}
+
+bool CEngine::ChangeTextureColor(const std::string& texName,
+                                 Color colorRef1, Color colorNew1,
+                                 Color colorRef2, Color colorNew2,
+                                 float tolerance1, float tolerance2,
+                                 Math::Point ts, Math::Point ti,
+                                 Math::Point *exclude, float shift, bool hsv)
+{
+    return ChangeTextureColor(texName, texName, colorRef1, colorNew1, colorRef2, colorNew2, tolerance1, tolerance2, ts, ti, exclude, shift, hsv);
 }
 
 void CEngine::DeleteTexture(const std::string& texName)
@@ -2832,7 +2842,6 @@ void CEngine::SetTextureFilterMode(TexFilter value)
     m_defaultTexParams.filter = m_terrainTexParams.filter = value;
     m_defaultTexParams.mipmap = m_terrainTexParams.mipmap = (value == TEX_FILTER_TRILINEAR);
     ResetAfterDeviceChanged();
-    CRobotMain::GetInstancePointer()->ResetAfterDeviceChanged();
 }
 
 TexFilter CEngine::GetTextureFilterMode()
@@ -2848,7 +2857,6 @@ void CEngine::SetTextureMipmapLevel(int value)
 
     m_textureMipmapLevel = value;
     ResetAfterDeviceChanged();
-    CRobotMain::GetInstancePointer()->ResetAfterDeviceChanged();
 }
 
 int CEngine::GetTextureMipmapLevel()
@@ -2865,7 +2873,6 @@ void CEngine::SetTextureAnisotropyLevel(int value)
 
     m_textureAnisotropy = value;
     ResetAfterDeviceChanged();
-    CRobotMain::GetInstancePointer()->ResetAfterDeviceChanged();
 }
 
 int CEngine::GetTextureAnisotropyLevel()

src/graphics/engine/engine.h
@@ -959,6 +959,15 @@ public:
     bool            LoadAllTextures();
 
     //! Changes colors in a texture
+    //@{
+    bool            ChangeTextureColor(const std::string& texName,
+                                       const std::string& srcName,
+                                       Color colorRef1, Color colorNew1,
+                                       Color colorRef2, Color colorNew2,
+                                       float tolerance1, float tolerance2,
+                                       Math::Point ts, Math::Point ti,
+                                       Math::Point *exclude = nullptr,
+                                       float shift = 0.0f, bool hsv = false);
     bool            ChangeTextureColor(const std::string& texName,
                                        Color colorRef1, Color colorNew1,
                                        Color colorRef2, Color colorNew2,
@@ -966,6 +975,7 @@ public:
                                        Math::Point ts, Math::Point ti,
                                        Math::Point *exclude = nullptr,
                                        float shift = 0.0f, bool hsv = false);
+    //@}
 
     //! Sets texture for given stage; if not present in cache, the texture is loaded
     /** If loading fails, returns false. */

src/graphics/engine/oldmodelmanager.cpp
@@ -23,6 +23,7 @@
 
 #include "common/logger.h"
 #include "common/resources/inputstream.h"
+#include "common/stringutils.h"
 
 #include "graphics/engine/engine.h"
 
@@ -42,7 +43,7 @@ COldModelManager::~COldModelManager()
 {
 }
 
-bool COldModelManager::LoadModel(const std::string& fileName, bool mirrored)
+bool COldModelManager::LoadModel(const std::string& fileName, bool mirrored, int variant)
 {
     GetLogger()->Debug("Loading model '%s'\n", fileName.c_str());
 
@@ -72,7 +73,10 @@ bool COldModelManager::LoadModel(const std::string& fileName, bool mirrored)
     if (mirrored)
         Mirror(modelInfo.triangles);
 
-    FileInfo fileInfo(fileName, mirrored);
+    if (variant != 0)
+        ChangeVariant(modelInfo.triangles, variant);
+
+    FileInfo fileInfo(fileName, mirrored, variant);
     m_models[fileInfo] = modelInfo;
 
     m_engine->AddBaseObjTriangles(modelInfo.baseObjRank, modelInfo.triangles);
@@ -80,15 +84,15 @@ bool COldModelManager::LoadModel(const std::string& fileName, bool mirrored)
     return true;
 }
 
-bool COldModelManager::AddModelReference(const std::string& fileName, bool mirrored, int objRank)
+bool COldModelManager::AddModelReference(const std::string& fileName, bool mirrored, int objRank, int variant)
 {
-    auto it = m_models.find(FileInfo(fileName, mirrored));
+    auto it = m_models.find(FileInfo(fileName, mirrored, variant));
     if (it == m_models.end())
     {
-        if (!LoadModel(fileName, mirrored))
+        if (!LoadModel(fileName, mirrored, variant))
             return false;
 
-        it = m_models.find(FileInfo(fileName, mirrored));
+        it = m_models.find(FileInfo(fileName, mirrored, variant));
     }
 
     m_engine->SetObjectBaseRank(objRank, (*it).second.baseObjRank);
@@ -96,15 +100,15 @@ bool COldModelManager::AddModelReference(const std::string& fileName, bool mirro
     return true;
 }
 
-bool COldModelManager::AddModelCopy(const std::string& fileName, bool mirrored, int objRank)
+bool COldModelManager::AddModelCopy(const std::string& fileName, bool mirrored, int objRank, int variant)
 {
-    auto it = m_models.find(FileInfo(fileName, mirrored));
+    auto it = m_models.find(FileInfo(fileName, mirrored, variant));
     if (it == m_models.end())
     {
-        if (!LoadModel(fileName, mirrored))
+        if (!LoadModel(fileName, mirrored, variant))
             return false;
 
-        it = m_models.find(FileInfo(fileName, mirrored));
+        it = m_models.find(FileInfo(fileName, mirrored, variant));
     }
 
     int copyBaseObjRank = m_engine->CreateBaseObject();
@@ -116,14 +120,14 @@ bool COldModelManager::AddModelCopy(const std::string& fileName, bool mirrored, 
     return true;
 }
 
-bool COldModelManager::IsModelLoaded(const std::string& fileName, bool mirrored)
+bool COldModelManager::IsModelLoaded(const std::string& fileName, bool mirrored, int variant)
 {
-    return m_models.count(FileInfo(fileName, mirrored)) > 0;
+    return m_models.count(FileInfo(fileName, mirrored, variant)) > 0;
 }
 
-int COldModelManager::GetModelBaseObjRank(const std::string& fileName, bool mirrored)
+int COldModelManager::GetModelBaseObjRank(const std::string& fileName, bool mirrored, int variant)
 {
-    auto it = m_models.find(FileInfo(fileName, mirrored));
+    auto it = m_models.find(FileInfo(fileName, mirrored, variant));
     if (it == m_models.end())
         return -1;
 
@@ -140,9 +144,9 @@ void COldModelManager::DeleteAllModelCopies()
     m_copiesBaseRanks.clear();
 }
 
-void COldModelManager::UnloadModel(const std::string& fileName, bool mirrored)
+void COldModelManager::UnloadModel(const std::string& fileName, bool mirrored, int variant)
 {
-    auto it = m_models.find(FileInfo(fileName, mirrored));
+    auto it = m_models.find(FileInfo(fileName, mirrored, variant));
     if (it == m_models.end())
         return;
 
@@ -177,5 +181,23 @@ void COldModelManager::Mirror(std::vector<ModelTriangle>& triangles)
     }
 }
 
+void COldModelManager::ChangeVariant(std::vector<ModelTriangle>& triangles, int variant)
+{
+    for (int i = 0; i < static_cast<int>( triangles.size() ); i++)
+    {
+        if (triangles[i].tex1Name == "base1.png"   ||
+            triangles[i].tex1Name == "convert.png" ||
+            triangles[i].tex1Name == "derrick.png" ||
+            triangles[i].tex1Name == "factory.png" ||
+            triangles[i].tex1Name == "lemt.png"    ||
+            triangles[i].tex1Name == "roller.png"  ||
+            triangles[i].tex1Name == "search.png"  ||
+            triangles[i].tex1Name == "drawer.png"  ||
+            triangles[i].tex1Name == "subm.png"     )
+        {
+            triangles[i].tex1Name += StrUtils::ToString<int>(variant);
+        }
+    }
 }
 
+}

src/graphics/engine/oldmodelmanager.h
@@ -58,31 +58,33 @@ public:
     ~COldModelManager();
 
     //! Loads a model from given file
-    bool LoadModel(const std::string& fileName, bool mirrored);
+    bool LoadModel(const std::string& fileName, bool mirrored, int variant = 0);
 
     //! Adds an instance of model to the given object rank as a reference to base object
-    bool AddModelReference(const std::string& fileName, bool mirrored, int objRank);
+    bool AddModelReference(const std::string& fileName, bool mirrored, int objRank, int variant = 0);
 
     //! Adds an instance of model to the given object rank as a copy (copied base object)
-    bool AddModelCopy(const std::string& fileName, bool mirrored, int objRank);
+    bool AddModelCopy(const std::string& fileName, bool mirrored, int objRank, int variant = 0);
 
     //! Returns true if given model is loaded
-    bool IsModelLoaded(const std::string& fileName, bool mirrored);
+    bool IsModelLoaded(const std::string& fileName, bool mirrored, int variant = 0);
 
     //! Returns the rank of base engine object of given loaded model
-    int GetModelBaseObjRank(const std::string& fileName, bool mirrored);
+    int GetModelBaseObjRank(const std::string& fileName, bool mirrored, int variant = 0);
 
     //! Deletes all copied objects
     void DeleteAllModelCopies();
 
     //! Unloads the given model
-    void UnloadModel(const std::string& fileName, bool mirrored);
+    void UnloadModel(const std::string& fileName, bool mirrored, int variant = 0);
     //! Unloads all models
     void UnloadAllModels();
 
 protected:
     //! Mirrors the model along the Z axis
     void Mirror(std::vector<ModelTriangle>& triangles);
+    //! Changes variant
+    void ChangeVariant(std::vector<ModelTriangle>& triangles, int variant);
 
 private:
     struct ModelInfo
@@ -94,10 +96,12 @@ private:
     {
         std::string fileName;
         bool mirrored;
+        int variant;
 
-        inline FileInfo(const std::string& _fileName, bool _mirrored)
+        inline FileInfo(const std::string& _fileName, bool _mirrored, int _variant = 0)
          : fileName(_fileName)
          , mirrored(_mirrored)
+         , variant(_variant)
         {}
 
         inline bool operator<(const FileInfo& other) const
@@ -108,6 +112,9 @@ private:
             if (compare > 0)
                 return false;
 
+            if (variant < other.variant)
+                return true;
+
             return !mirrored && mirrored != other.mirrored;
         }
     };
@@ -117,4 +124,3 @@ private:
 };
 
 } // namespace Gfx
-

src/object/auto/autofactory.cpp
@@ -27,6 +27,7 @@
 
 #include "object/brain.h"
 #include "object/old_object.h"
+#include "object/object_create_params.h"
 #include "object/object_manager.h"
 #include "object/robotmain.h"
 #include "object/level/parserline.h"
@@ -643,11 +644,15 @@ bool CAutoFactory::CreateVehicle()
     Math::Matrix* mat = m_object->GetWorldMatrix(0);
     pos = Transform(*mat, pos);
 
-    CObject* vehicle = CObjectManager::GetInstancePointer()->CreateObject(pos, angle, m_type);
+    ObjectCreateParams params;
+    params.pos = pos;
+    params.angle = angle;
+    params.type = m_type;
+    params.team = m_object->GetTeam();
+    CObject* vehicle = CObjectManager::GetInstancePointer()->CreateObject(params);
 
     vehicle->SetLock(true);  // not usable
     vehicle->SetRange(30.0f);
-    vehicle->SetTeam(m_object->GetTeam());
 
     CPhysics* physics = vehicle->GetPhysics();
     if ( physics != nullptr )

commit cba3863d75c0d62ace10c44b1dfa20fe31cb38e5

Fixed bug with object loading introduced in 083f21652949d48fc1c9dada0cfb8937e9ee2b71

src/object/old_object.cpp
@@ -1006,7 +1006,6 @@ void COldObject::Read(CLevelParserLine* line)
     SetProxyDistance(line->GetParam("proxyDistance")->AsFloat(15.0f)*g_unit);
     SetRange(line->GetParam("range")->AsFloat(30.0f));
     SetMagnifyDamage(line->GetParam("magnifyDamage")->AsFloat(1.0f));
-    SetTeam(line->GetParam("team")->AsInt(0));
     SetGunGoalV(line->GetParam("aimV")->AsFloat(0.0f));
     SetGunGoalH(line->GetParam("aimH")->AsFloat(0.0f));
 

src/object/robotmain.cpp
@@ -5065,8 +5065,9 @@ CObject* CRobotMain::IOReadObject(CLevelParserLine *line, const char* filename, 
     bool trainer = line->GetParam("trainer")->AsBool(false);
     bool toy = line->GetParam("toy")->AsBool(false);
     int option = line->GetParam("option")->AsInt(0);
+    int team = line->GetParam("team")->AsInt(0);
 
-    CObject* obj = m_objMan->CreateObject(pos, dir.y, type, 0.0f, 1.0f, 0.0f, trainer, toy, option, id);
+    CObject* obj = m_objMan->CreateObject(pos, dir.y, type, 0.0f, 1.0f, 0.0f, trainer, toy, option, team, id);
 
     if (obj->Implements(ObjectInterfaceType::Old))
     {