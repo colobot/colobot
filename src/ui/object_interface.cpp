0986a200d7b64ddba4ec7dad046f27d5ad9385f6
krzys-h <krzys_h@interia.pl>
2015-08-16 16:03:36 +0200

    Fixed CBot editor being inaccessible while running program

--- src/ui/object_interface.cpp
+++ src/ui/object_interface.cpp
@@ -1622,7 +1622,7 @@ void CObjectInterface::UpdateInterface()
     bool bProgEnable = !m_programmable->IsProgram() && m_main->CanPlayerInteract();
     bool scriptSelected = m_selScript >= 0 && m_selScript < m_programStorage->GetProgramCount();
 
-    EnableInterface(pw, EVENT_OBJECT_PROGEDIT,    bProgEnable && !m_programmable->IsTraceRecord() && scriptSelected);
+    EnableInterface(pw, EVENT_OBJECT_PROGEDIT,    m_main->CanPlayerInteract() && scriptSelected && !m_programmable->IsTraceRecord());
     EnableInterface(pw, EVENT_OBJECT_PROGLIST,    bProgEnable && !m_programmable->IsTraceRecord());
     EnableInterface(pw, EVENT_OBJECT_PROGADD,     bProgEnable);
     EnableInterface(pw, EVENT_OBJECT_PROGREMOVE,  bProgEnable && scriptSelected && !m_programStorage->GetProgram(m_selScript)->readOnly);
