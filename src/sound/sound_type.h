5542b7deb783ca540ba1bec75ba90d4f8858aca9
Cezary Parkowski <raptor.oto@gmail.com>
2016-02-19 17:43:55 +0100

    Another fixes to sound samples in-code documentation

--- src/sound/sound_type.h
+++ src/sound/sound_type.h
@@ -33,7 +33,7 @@
 **/
 enum SoundType
 {
-  SOUND_NONE = -1,      /*!< Mute. */
+  SOUND_NONE = -1,      /*!< Silence. */
   SOUND_CLICK = 0,      /*!< Keybinding change. */
   SOUND_BOUM  = 1,      /*!< Landing on ground bump. */
   SOUND_EXPLO  = 2,     /*!< Building explosion. */
@@ -45,27 +45,27 @@ enum SoundType
   SOUND_MOTORr  = 8,    /*!< SpecialBot engine. */
   SOUND_ERROR  = 9,     /*!< DisplayError message. */
   SOUND_CONVERT  = 10,  /*!< Converter processing. */
-  SOUND_ENERGY  = 11,   /*!< PowerStation recharging #1/Sparkle in menu #1. */
+  SOUND_ENERGY  = 11,   /*!< PowerStation recharging #1/Menu particle #1. */
   SOUND_PLOUF  = 12,    /*!< Water splash. */
   SOUND_BLUP  = 13,     /*!< Swimming #1. */
   SOUND_WARNING  = 14,  /*!< DisplayWarning message. */
   SOUND_DERRICK  = 15,  /*!< Derrick digging. */
   SOUND_LABO  = 16,     /*!< AutoLab processing sample. */
-  SOUND_STATION  = 17,  /*!< PowerStation recharging #2/Sparkle in menu #2. */
-  SOUND_REPAIR  = 18,   /*!< RepairCenter/Sniffer working. */
+  SOUND_STATION  = 17,  /*!< PowerStation recharging #2/Menu particle #2. */
+  SOUND_REPAIR  = 18,   /*!< RepairCenter/Sniffer sniffing. */
   SOUND_RESEARCH  = 19, /*!< ResearchCenter processing. */
   SOUND_INSECTs  = 20,  /*!< AlienSpider roar. */
   SOUND_BURN  = 21,     /*!< Burning. */
   SOUND_TZOING  = 22,   /*!< EPSITEC's Brand "TZOING!". */
-  SOUND_GGG  = 23,      /*!< Electricity/Menu particle sound #2. */
-  SOUND_MANIP  = 24,    /*!< Bot's mechanic arm manipulating. */
-  SOUND_FIRE  = 25,     /*!< ShooterBots shooting. */
+  SOUND_GGG  = 23,      /*!< Electricity/Menu particle #3. */
+  SOUND_MANIP  = 24,    /*!< Bot mechanic arm manipulating. */
+  SOUND_FIRE  = 25,     /*!< ShooterBot shooting. */
   SOUND_HUMAN1  = 26,   /*!< Astronaut breathing. */
   SOUND_STEPw  = 27,    /*!< Step under the water. */
   SOUND_SWIM  = 28,     /*!< Swimming #2. */
-  SOUND_RADAR  = 29,    /*!< RadarStation "DING!"/Sparkle in menu #5. */
-  SOUND_BUILD  = 30,    /*!< Building processing */
-  SOUND_ALARM  = 31,    /*!< Bot energy alarm/Sparkle in menu #3. */
+  SOUND_RADAR  = 29,    /*!< RadarStation "DING!"/Menu particle #4. */
+  SOUND_BUILD  = 30,    /*!< Building processing. */
+  SOUND_ALARM  = 31,    /*!< Bot energy alarm/Menu particle #5. */
   SOUND_SLIDE  = 32,    /*!< WingedBot engine. */
   SOUND_EXPLOi  = 33,   /*!< AlienInsect exploding scream. */
   SOUND_INSECTa  = 34,  /*!< AlienAnt roar. */
@@ -73,20 +73,20 @@ enum SoundType
   SOUND_INSECTw  = 36,  /*!< AlienWorm roar. */
   SOUND_INSECTm  = 37,  /*!< AlienQueen roar. */
   SOUND_TREMBLE  = 38,  /*!< Building ambient. */
-  SOUND_PSHHH  = 39,    /*!< SpecialBot engine "PSHHH!"/Menu particle sound #1. */
+  SOUND_PSHHH  = 39,    /*!< SpecialBot engine "PSHHH!"/Menu particle #6. */
   SOUND_NUCLEAR  = 40,  /*!< NuclearPlant processing. */
-  SOUND_INFO  = 41,     /*!< DisplayInfo message/Sparkle in menu #4. */
+  SOUND_INFO  = 41,     /*!< DisplayInfo message/Menu particle #7. */
   SOUND_OPEN  = 42,     /*!< Converter open/close. */
   SOUND_CLOSE  = 43,    /*!< Converter slam. */
   SOUND_FACTORY  = 44,  /*!< BotFactory processing. */
   SOUND_EGG  = 45,      /*!< AlienEgg breaking eggshell. */
   SOUND_MOTORs  = 46,   /*!< Subber engine. */
   SOUND_MOTORi  = 47,   /*!< LeggedBot engine. */
-  SOUND_SHIELD  = 48,   /*!< Shielder working. */
-  SOUND_FIREi  = 49,    /*!< OrgaShooterBots shooting. */
+  SOUND_SHIELD  = 48,   /*!< Shielder shielding. */
+  SOUND_FIREi  = 49,    /*!< OrgaShooterBot shooting. */
   SOUND_GUNDEL  = 50,   /*!< Neutralized AlienInsect's fire under the shield. */
   SOUND_PSHHH2  = 51,   /*!< Shielder mechanic tower manipulating. */
-  SOUND_MESSAGE  = 52,  /*!< DisplayMessage message */
+  SOUND_MESSAGE  = 52,  /*!< DisplayMessage message. */
   SOUND_BOUMm  = 53,    /*!< Metal bang. */
   SOUND_BOUMv  = 54,    /*!< Plant bang. */
   SOUND_BOUMs  = 55,    /*!< Smooth bang. */
@@ -98,23 +98,23 @@ enum SoundType
   SOUND_POWERON  = 61,  /*!< Power on. */
   SOUND_POWEROFF  = 62, /*!< Power off. */
   SOUND_AIE  = 63,      /*!< Astronaut pain "AIE!". */
-  SOUND_WAYPOINT  = 64, /*!< WayPoint/Target2 activate, placing a flag. */
+  SOUND_WAYPOINT  = 64, /*!< WayPoint/Target2 activate/Flag placing. */
   SOUND_RECOVER  = 65,  /*!< Recycler sound. */ /* DUPLICATE OF SOUND_GUNDEL=50 */
   SOUND_DEADi  = 66,    /*!< AlienInsect dying scream. */
   SOUND_JOSTLE  = 67,   /*!< Leaf shaking. */
-  SOUND_GFLAT  = 68,    /*!< FlatGround probing/Sparkle in menu #6. */
+  SOUND_GFLAT  = 68,    /*!< FlatGround probing/Menu particle #8. */
   SOUND_DEADg  = 69,    /*!< Astronaut death "AU!". */
   SOUND_DEADw  = 70,    /*!< Astronaut drown "AHU!AHU!". */
-  SOUND_FLYf  = 71,     /*!< Jet engine Overheating. */
-  SOUND_ALARMt  = 72,   /*!< Bot temperature alarm/Sparkle in menu #7. */
-  SOUND_FINDING  = 73,  /*!< ProxyActivating an object, opening Vault. */ /* DUPLICATE OF SOUND_WAYPOINT=64 */
+  SOUND_FLYf  = 71,     /*!< Jet engine overheating. */
+  SOUND_ALARMt  = 72,   /*!< Bot temperature alarm/Menu particle #9. */
+  SOUND_FINDING  = 73,  /*!< Vault opening/ProxyActivating an object. */ /* DUPLICATE OF SOUND_WAYPOINT=64 */
   SOUND_THUMP  = 74,    /*!< Thumper thumping. */
   SOUND_TOUCH  = 75,    /*!< Bot getting damage. */
   SOUND_BLITZ  = 76,    /*!< Thunder. */
   SOUND_MUSHROOM  = 77, /*!< Mushroom poisoning. */
   SOUND_FIREp  = 78,    /*!< PhazerShooter shooting. */
-  SOUND_EXPLOg1  = 79,  /*!< ShooterBots damaging things. */
-  SOUND_EXPLOg2  = 80,  /*!< OrgaShooterBots damaging things. */
+  SOUND_EXPLOg1  = 79,  /*!< ShooterBots damaging objects. */
+  SOUND_EXPLOg2  = 80,  /*!< OrgaShooterBots damaging objects. */
   SOUND_MOTORd  = 81,   /*!< Scribbler/ToyBot engine. */
   SOUND_MAX             /** number of items in enum */
 };
