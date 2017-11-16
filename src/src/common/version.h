b04d8ca99d16669ee71655a0145e6fcdcbb9867e
Fiftytwo <emxx52@gmail.com>
2017-11-16 20:32:10 +0100

    Clean up redundant files

--- src/src/common/version.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define COLOBOT_FULLNAME "Colobot: Gold Edition 0.1.11+alpha-git-dev~radda8281"
-#define COLOBOT_VERSION_DISPLAY "git-dev~radda8281"
-
-#define BUILD_NUMBER 0
-/* #undef OFFICIAL_BUILD */
