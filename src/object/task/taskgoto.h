1a771b0e72d44e6de2e6df74eb0fe71f7ee612d5
pkubaj <pkubaj@users.noreply.github.com>
2019-10-18 14:00:37 +0200

    Fix build on ARM and PPC
    ARM and PPC use unsigned char by default.

--- src/object/task/taskgoto.h
+++ src/object/task/taskgoto.h
@@ -147,7 +147,7 @@ protected:
     int             m_bmTotal = 0;      // number of points in m_bmPoints
     int             m_bmIndex = 0;      // index in m_bmPoints
     Math::Vector        m_bmPoints[MAXPOINTS+2];
-    char            m_bmIter[MAXPOINTS+2] = {};
+    signed char     m_bmIter[MAXPOINTS+2] = {};
     int             m_bmIterCounter = 0;
     CObject*        m_bmCargoObject = nullptr;
     float           m_bmFinalMove = 0.0f;  // final advance distance
