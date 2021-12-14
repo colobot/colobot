36ac6d99561c6d1de47990065f714201d1dfe6d2
Tomasz Kapuściński <tomaszkax86@gmail.com>
2021-12-15 00:09:47 +0100

    Another fix

--- src/graphics/opengl/gl33objectrenderer.cpp
+++ src/graphics/opengl/gl33objectrenderer.cpp
@@ -28,8 +28,7 @@
 
 #include <GL/glew.h>
 
-#include <glm/ext/matrix_common.hpp>
-#include <glm/ext/matrix_transform.hpp>
+#include <glm/ext.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
 using namespace Gfx;
--- src/graphics/opengl/gl33renderers.cpp
+++ src/graphics/opengl/gl33renderers.cpp
@@ -9,7 +9,7 @@
 
 #include <GL/glew.h>
 
-#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
 namespace Gfx
