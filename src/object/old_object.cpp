f672e8920127a501fbcc09c83647e4df90d0e61c
tomangelo2 <tomangelo@wp.pl>
2023-06-22 23:32:02 +0200

    Better fix for #1587

--- src/object/old_object.cpp
+++ src/object/old_object.cpp
@@ -1728,7 +1728,8 @@ Math::Vector COldObject::GetSlotPosition(int slotNum)
             grabRelPos = Math::Vector(4.7f, 0.0f, 0.0f);
             break;
         case OBJECT_BEE:
-            grabPartNum = 7;
+            grabPartNum = 0;
+            grabRelPos = Math::Vector(0.0f, -3.0f, 0.0f);
             break;
         default: // unreachable, only the above objects have cargo slots
             assert(!m_hasCargoSlot);
