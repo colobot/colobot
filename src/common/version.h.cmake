d9d401e63648dafac304c161e474d8f27b515776
Tomasz Kapuściński <tomaszkax86@gmail.com>
2023-08-13 17:56:16 +0200

    Removed extra semicolon

--- src/common/version.h.cmake
+++ src/common/version.h.cmake
@@ -11,4 +11,4 @@ namespace Version
     static inline constexpr std::string_view FULL_NAME = "Colobot: Gold Edition @COLOBOT_VERSION_FULL@";
     static inline constexpr std::string_view VERSION = "@CMAKE_PROJECT_VERSION@";
     static inline constexpr std::string_view VERSION_DISPLAY = "@COLOBOT_VERSION_DISPLAY@";
-};
+}
