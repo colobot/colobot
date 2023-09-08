9bcd05cbddc9d45e1fcd3ad5ee55292bf747294c
Tomasz Kapuściński <tomaszkax86@gmail.com>
2023-09-09 00:58:52 +0200

    Added development mode that copies built files to build directory

--- colobot-base/src/common/version.h.cmake
+++ colobot-base/src/common/version.h.cmake
@@ -5,7 +5,7 @@
 namespace Version
 {
     static inline constexpr bool OFFICIAL_BUILD = @OFFICIAL_COLOBOT_BUILD@;
-    static inline constexpr bool DEVELOPMENT_BUILD = @DEVELOPMENT_COLOBOT_BUILD@;
+    static inline constexpr bool DEVELOPMENT_BUILD = @COLOBOT_DEVELOPMENT_MODE@;
     static inline constexpr int BUILD_NUMBER = @BUILD_NUMBER@;
 
     static inline constexpr std::string_view FULL_NAME = "Colobot: Gold Edition @COLOBOT_VERSION_FULL@";
