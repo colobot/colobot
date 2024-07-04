cd861a20b26094c7d536bd7ba83d009cfe0981fd
hexagon-recursion <hexagon-recursion@posteo.net>
2024-07-04 15:37:00 +0300

    Loading old save files is unsupported

--- colobot-base/src/common/version.h.cmake
+++ colobot-base/src/common/version.h.cmake
@@ -13,5 +13,9 @@ namespace Version
 
     static inline constexpr std::string_view FULL_NAME = "Colobot: Gold Edition @COLOBOT_VERSION_FULL@";
     static inline constexpr std::string_view VERSION = "@CMAKE_PROJECT_VERSION@";
+    static inline constexpr int MAJOR = @CMAKE_PROJECT_VERSION_MAJOR@;
+    static inline constexpr int MINOR = @CMAKE_PROJECT_VERSION_MINOR@;
+    static inline constexpr int PATCH = @CMAKE_PROJECT_VERSION_PATCH@;
+    static inline constexpr int TWEAK = @CMAKE_PROJECT_VERSION_TWEAK@;
     static inline constexpr std::string_view VERSION_DISPLAY = "@COLOBOT_VERSION_DISPLAY@";
 }
