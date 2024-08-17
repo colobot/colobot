3be1b3908336d2581e3c42f95de059c0803f11b2
hexagon-recursion <hexagonrecursion@gmail.com>
2024-08-17 20:53:04 +0300

    Fix proxyActivate on objects with a power cell

--- src/level/robotmain.cpp
+++ src/level/robotmain.cpp
@@ -1993,13 +1993,14 @@ CObject* CRobotMain::DetectObject(Math::Point pos)
     for (CObject* obj : m_objMan->GetAllObjects())
     {
         if (!obj->GetDetectable()) continue;
+        if (obj->GetProxyActivate()) continue;
 
         CObject* transporter = nullptr;
         if (obj->Implements(ObjectInterfaceType::Transportable))
             transporter = dynamic_cast<CTransportableObject&>(*obj).GetTransporter();
 
         if (transporter != nullptr && !transporter->GetDetectable()) continue;
-        if (obj->GetProxyActivate()) continue;
+        if (transporter != nullptr && transporter->GetProxyActivate()) continue;
 
         CObject* target = obj;
         // TODO: should this also apply to slots other than power cell slots?
