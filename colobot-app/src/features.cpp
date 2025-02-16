1f0edd48edfddb79ff03cd9892997bc995f31f10
Tomasz Kapuściński <tomaszkax86@gmail.com>
2025-02-16 14:55:23 +0100

    Added test for designated initializers

--- colobot-app/src/features.cpp
+++ colobot-app/src/features.cpp
@@ -1,3 +1,13 @@
 #include <version>
 
-//
+// Designated initializers
+void test_di()
+{
+    struct Foo
+    {
+        int x;
+        float y;
+    };
+
+    Foo foo = { .x = 23, .y = 34.5f };
+}
