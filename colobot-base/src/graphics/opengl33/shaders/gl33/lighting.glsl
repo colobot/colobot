d3d3070bb66b42de948344bb40be23d634ddfc97
Tomasz Kapuściński <tomaszkax86@gmail.com>
2025-11-11 17:30:27 +0100

    Fixed calculation

--- colobot-base/src/graphics/opengl33/shaders/gl33/lighting.glsl
+++ colobot-base/src/graphics/opengl33/shaders/gl33/lighting.glsl
@@ -42,8 +42,8 @@ vec3 SchlickFresnel(float LdH, float metalness, vec3 color)
 float GeometrySmith(float dotProd, float roughness)
 {
     float k = (roughness + 1.0) * (roughness + 1.0) / 8.0;
-    float denom = dotProd * (1 - k) + k;
-    return 1.0 / denom;
+
+    return dotProd / (dotProd * (1 - k) + k);
 }
 
 float GGXDistribution(float NdH, float roughness)
